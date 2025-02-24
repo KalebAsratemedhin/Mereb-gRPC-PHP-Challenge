4c367a3a017f7380d593e95ca827f619474b0f52
KalebAsratemedhin <risekab@gmail.com>
2025-02-24 12:56:56 +0300

    feat: update styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,17 +15,21 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>gRPC Ping Test</h1>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder="Enter a message"
-      />
-      <button onClick={handlePing}>Ping</button>
-      <h2>Response:</h2>
-      <p>{response}</p>
+    <div className='home '>
+      <div className='container'>
+        <h1>gRPC Ping Test</h1>
+        <p style={{ marginBottom: "30px"}}>This application demos a php grpc server implemented using spiral/php-grpc. The grpc client setup with react communicates with the php server using an envoy.</p>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Enter a message"
+          style={{ marginBottom: "30px", padding: "8px", fontSize: "25px", fontFamily: "Poppins", borderRadius: "5px", border: "1px solid gray"}}
+        />
+        <button onClick={handlePing}>Ping</button>
+        <h2>Response:</h2>
+        <p>{response}</p>
+      </div>
     </div>
   );
 }
